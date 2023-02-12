bb6abcc3af4fa776b347983526e97b374eaf4ae1
Ruy Monteiro <ruystfrancis@gmail.com>
2023-02-12 18:28:23 -0300

    Not found mobile

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,9 +4,9 @@ import lostSailor from "../assets/lost-sailor.svg";
 export function NotFound() {
   return (
     <main className="w-full h-screen max-h-full overflow-hidden flex items-center justify-between">
-      <img src={lostSailor} alt="Lost Sailor Illustration" className="-ml-48 grow overflow-hidden" />
+      <img src={lostSailor} alt="Lost Sailor Illustration" className="-ml-48 grow overflow-hidden hidden md:flex" />
 
-      <div className="h-full flex flex-col items-start gap-12 p-8 justify-center w-[40%]">
+      <div className="h-full flex flex-col items-start gap-12 p-8 justify-center w-full md:w-[40%]">
         <div className="flex flex-col gap-6">
           <h1 className="text-6xl font-extrabold text-teal-500">404</h1>
           <h2 className="text-4xl font-bold text-teal-500">Onde estamos, Capitão?!</h2>
