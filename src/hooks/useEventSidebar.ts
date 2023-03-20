469de586144c917ff555748a4534638d750fa436
Ruy Monteiro <ruystfrancis@gmail.com>
2023-03-20 09:56:42 -0300

    Atualizar o useEventSidebar.ts

--- src/hooks/useEventSidebar.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { SideDrawerContext } from '../contexts/SideDrawerContext';
-
-export function useEventSidebar() {
-  const { setContent, content, isOpen, setIsOpen } = useContext(SideDrawerContext);
-  return { setContent, content, isOpen, setIsOpen };
-}
